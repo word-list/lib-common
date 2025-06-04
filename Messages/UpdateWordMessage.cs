6ecd322c14d412ad36255a836665c296bdc5e694
Jonathan Gaul <jon@gaul.tech>
2025-06-04 20:32:04 +0000

    Add UpdateWordMessage

---
+++ Messages/UpdateWordMessage.cs
@@ -0,0 +1,10 @@
+namespace WordList.Common.Messages;
+
+public class UpdateWordMessage
+{
+    public required string Word { get; set; }
+    public int Offensiveness { get; set; }
+    public int Commonness { get; set; }
+    public int Sentiment { get; set; }
+    public string[] WordTypes { get; set; } = [];
+}
