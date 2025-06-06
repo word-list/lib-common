059ad21379b9f0a57eef90ecb8f435ae7777b41d
Jonathan Gaul <jon@gaul.tech>
2025-06-06 06:10:19 +0000

    Allow MessageQueue to be used without a URL, add SendBatchedMessagesAsync helper function

--- Messaging/MessageQueue.cs
+++ Messaging/MessageQueue.cs
@@ -10,8 +10,13 @@ namespace WordList.Common.Messaging;
 
 public class MessageQueue<T>
 {
+    /* For callers who are only receiving from Lambda input (i.e. SQSEvent), a queue URL
+     * is not required.  This allows us to store the name of the environment variable we'd
+     * use and only actually use it if needed.
+     */
+    private string? _environmentVariable;
     private readonly static AmazonSQSClient s_sqs = new();
-    public string TargetUrl { get; init; }
+    public string TargetUrl { get; private set; }
     public JsonTypeInfo<T> JsonTypeInfo { get; init; }
 
 
@@ -21,8 +26,28 @@ public class MessageQueue<T>
         JsonTypeInfo = jsonTypeInfo;
     }
 
+    public static MessageQueue<U> FromEnvironmentVariable<U>(string name, JsonTypeInfo<U> jsonTypeInfo)
+        => new("", jsonTypeInfo) { _environmentVariable = name };
+
+    public async Task SendBatchedMessagesAsync(ILogger logger, IEnumerable<T> messages)
+    {
+        var sender = GetBatchSender(logger);
+        sender.AddRange(messages);
+        await sender.SendAllMessagesAsync().ConfigureAwait(false);
+    }
+
     public BatchedMessageSender<T> GetBatchSender(ILogger logger)
-        => new(s_sqs, TargetUrl, JsonTypeInfo, logger);
+    {
+        if (string.IsNullOrEmpty(TargetUrl))
+        {
+            if (string.IsNullOrEmpty(_environmentVariable))
+                throw new Exception("Cannot send to a queue created with empty target URL and no environment variable");
+
+            TargetUrl = Environment.GetEnvironmentVariable(_environmentVariable) ?? throw new Exception($"{_environmentVariable} must be defined (cannot send to queue)");
+        }
+
+        return new(s_sqs, TargetUrl, JsonTypeInfo, logger);
+    }
 
     public T[] Receive(SQSEvent input, ILogger log)
     {
--- Messaging/MessageQueues.cs
+++ Messaging/MessageQueues.cs
@@ -6,32 +6,24 @@ namespace WordList.Common.Messaging;
 
 public static class MessageQueues
 {
-    static MessageQueue<U> GetQueueFromEnvironmentVariable<U>(string env, JsonTypeInfo<U> jsonTypeInfo)
-    {
-        var targetUrl = Environment.GetEnvironmentVariable(env)
-            ?? throw new Exception($"{env} must be defined");
-
-        return new(targetUrl, jsonTypeInfo);
-    }
-
     private static MessageQueue<UpdateWordMessage>? s_updateWords;
     public static MessageQueue<UpdateWordMessage> UpdateWords
-        => s_updateWords ??= GetQueueFromEnvironmentVariable("UPDATE_WORDS_QUEUE_URL", MessageSerializerContext.Default.UpdateWordMessage);
+        => s_updateWords ??= MessageQueue<UpdateWordMessage>.FromEnvironmentVariable("UPDATE_WORDS_QUEUE_URL", MessageSerializerContext.Default.UpdateWordMessage);
 
     private static MessageQueue<UpdateBatchMessage>? s_updateBatch;
     public static MessageQueue<UpdateBatchMessage> UpdateBatch
-        => s_updateBatch ??= GetQueueFromEnvironmentVariable("UPDATE_BATCH_QUEUE_URL", MessageSerializerContext.Default.UpdateBatchMessage);
+        => s_updateBatch ??= MessageQueue<UpdateBatchMessage>.FromEnvironmentVariable("UPDATE_BATCH_QUEUE_URL", MessageSerializerContext.Default.UpdateBatchMessage);
 
     private static MessageQueue<ProcessSourceChunkMessage>? s_processSourceChunk;
     public static MessageQueue<ProcessSourceChunkMessage> ProcessSourceChunk
-        => s_processSourceChunk ??= GetQueueFromEnvironmentVariable("PROCESS_SOURCE_CHUNK_QUEUE_URL", MessageSerializerContext.Default.ProcessSourceChunkMessage);
+        => s_processSourceChunk ??= MessageQueue<ProcessSourceChunkMessage>.FromEnvironmentVariable("PROCESS_SOURCE_CHUNK_QUEUE_URL", MessageSerializerContext.Default.ProcessSourceChunkMessage);
 
     private static MessageQueue<QueryWordMessage>? s_queryWords;
     public static MessageQueue<QueryWordMessage> QueryWords
-        => s_queryWords ??= GetQueueFromEnvironmentVariable("QUERY_WORDS_QUEUE_URL", MessageSerializerContext.Default.QueryWordMessage);
+        => s_queryWords ??= MessageQueue<QueryWordMessage>.FromEnvironmentVariable("QUERY_WORDS_QUEUE_URL", MessageSerializerContext.Default.QueryWordMessage);
 
     private static MessageQueue<UploadSourceChunksMessage>? s_uploadSourceChunks;
     public static MessageQueue<UploadSourceChunksMessage> UploadSourceChunks
-        => s_uploadSourceChunks ??= GetQueueFromEnvironmentVariable("UPLOAD_SOURCE_CHUNKS_QUEUE_URL", MessageSerializerContext.Default.UploadSourceChunksMessage);
+        => s_uploadSourceChunks ??= MessageQueue<UploadSourceChunksMessage>.FromEnvironmentVariable("UPLOAD_SOURCE_CHUNKS_QUEUE_URL", MessageSerializerContext.Default.UploadSourceChunksMessage);
 
 }
