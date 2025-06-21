c612dc48754bea2d21f9676a5acfdaa93463fb35
Jonathan Gaul <jon@gaul.tech>
2025-06-21 16:12:41 +0000

    Change UpdateWord message to new attributes model

--- Messaging/Messages/UpdateWordMessage.cs
+++ Messaging/Messages/UpdateWordMessage.cs
@@ -3,14 +3,7 @@ namespace WordList.Common.Messaging.Messages;
 public class UpdateWordMessage
 {
     public required string Word { get; set; }
-    public int Offensiveness { get; set; }
-    public int Commonness { get; set; }
-    public int Sentiment { get; set; }
     public string[] WordTypes { get; set; } = [];
 
-    public int Formality { get; set; }
-    public int CulturalSensitivity { get; set; }
-    public int Figurativeness { get; set; }
-    public int Complexity { get; set; }
-    public int Political { get; set; }
+    public Dictionary<string, int> Attributes { get; set; } = [];
 }
